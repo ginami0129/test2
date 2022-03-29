57933261a6ed23b94dee8be00fbb9b51e7e58429
Ginam Kwon <ginami0129g@gmail.com>
2022-03-29 11:55:35 +0900

    Add cors and morgan

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(cors);
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({
