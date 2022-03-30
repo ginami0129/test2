4c1154b13fbbc92704c97c94855a4a34fb0d5f66
Ginam Kwon <ginami0129g@gmail.com>
2022-03-30 16:50:47 +0900

    Update orderRoute post method: check order info

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -9,8 +9,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  const newOrder = {
+    product: req.body.product,
+    quantity: req.body.quantity,
+    msg: req.body.msg,
+  };
   res.json({
     msg: "Create a order",
+    order: newOrder,
   });
 });
 
